008f906f76dcfdaace36f826196e3caffbe9247b
Ana Gomez <haymenudana@hotmail.com>
2014-05-09 16:59:05 +0100

    Cambiado

--- src/solucion2.py
+++ src/solucion2.py
@@ -1,62 +1,44 @@
-#!/usr/bin/python
-#! encoding: UTF-8
+#! encoding: utf8
+# Ya puedo usar el español
 
-import time
-import timeit
-import ej_PI3
+import matplotlib.pylab as pl 
+import numpy as np
+import math
 
+x=int(raw_input('kjzdb: '))
+h=int(raw_input('kjzdb: '))
+l=[]
+for i in range (x):
+  y=1/np.sin(x)
+  l.append(y)
+  
+pl.figure(figsize=(8,6), dpi=80)
 
-lista=[]
+pl.subplot(1,1,1)
 
-def error(intervalos,test,umbral):
-  fallos=0
-  for i in range (test):
-    s=ej_PI3.h(intervalos)
-    error=abs(s-ej_PI3.PI)
-    if error>=umbral:
-      fallos=fallos+1
-  return ((fallos/test)*100)
+X = np.linspace(-x, x, 256, endpoint=True)
+C = 0*(X)
+S = 1/np.sin(X)
 
-if __name__=="__main__":
-   import sys
-   
-   if((len(sys.argv) == 1) or (len(sys.argv) == 4)):
-     print("No ha introducido todos los argumentos")
-     n= int(raw_input('Introduzca el valor n: ')) 
-     m= int(raw_input('Introduzca el valor m: '))
-     p= int(raw_input('Introduzca el valor p: '))
-     
-   else:
-     n=int(sys.argv[1])
-     m=int(sys.argv[2])
-     p=float(sys.argv[3])
-     
-   x = (10, 50, 100, 150, 500, 550, 1000)
-   l_tiempo= []
-   for i in x:
-     start=time.time()
-     s=error(i,m,p)
-     finish=time.time()-start
-     l_tiempo=l_tiempo+[finish]
-     print "El porcentaje de error es: %5.3f" %s
-     print "El tiempo que tardó el proceso es de: %14.13f" %finish
-     
-   lista=lista+[finish]
-   print "Proporcione un nombre para el fichero donde se guardarán los resultados:"
-   nombre_fichero= raw_input();
-   h=open(nombre_fichero, 'w')
-   h.write('El tiempo final es:')
-   h.write(str(lista[0]) + '\n')
-   h.close()
-   
-import matplotlib.pyplot as pl
+pl.plot(X,C, color="black", linewidth=1.0, linestyle="-", label="Eje de las X")
+pl.plot(X,S, color="yellow", linewidth=1.5, linestyle="-", label="arcoseno")
 
-pl.plot(x,l_tiempo, '--x')
-pl.title('Tiempo')
-pl.xlabel('Intervalos')
-pl.ylabel('Tiempo en segundos')
-pl.xticks([10, 50, 100, 150, 500, 550, 1000])
-pl.xlim(-10.0, 1100.0)
-pl.ylim(-0.001, 0.014)
-pl.savefig("Tiempo.eps", dpi=72)
+pl.legend(loc='upper left')
+
+#pl.xlim(-4.0,4.0)
+pl.xlim(X.min()*1.1,X.max()*1.1)
+
+#pl.xticks(np.linspace(-4,4,9,endpoint=True))
+pl.xticks([-x, x])
+
+#pl.ylim(-1.0,1.0)
+pl.ylim(C.min()*1.1,C.max()*1.1)
+
+#pl.yticks(np.linspace(-1,1,5,endpoint=True))
+pl.yticks([-h, h])
+
+pl.title("Representacion grafica")
+
+pl.savefig("sencos.eps", dpi=72)
+
 pl.show()
