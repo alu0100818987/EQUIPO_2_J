4c53a1818b8759d83ca4441aa9b2a759271a01de
Ana Gomez <alu0100818987@ull.edu.es>
2014-05-10 19:54:56 +0100

    beamer

--- src/solucion.py
+++ src/solucion.py
@@ -7,7 +7,6 @@ import time
 import matplotlib.pylab as pl 
 import numpy as np
 
-
 def factorial(n):
    if n <= 1:
      return 1
@@ -61,32 +60,17 @@ for i in range (g):
   l.append(y)
   
 pl.figure(figsize=(8,6), dpi=80)
-
 pl.subplot(1,1,1)
-
 X = np.linspace(-g, g, 256, endpoint=True)
 C = 0*(X)
 S = 1/np.sin(X)
-
 pl.plot(X,C, color="black", linewidth=1.0, linestyle="-", label="Eje de las X")
 pl.plot(X,S, color="yellow", linewidth=1.5, linestyle="-", label="arcoseno")
-
 pl.legend(loc='upper left')
-
-#pl.xlim(-4.0,4.0)
 pl.xlim(X.min()*1.1,X.max()*1.1)
-
-#pl.xticks(np.linspace(-4,4,9,endpoint=True))
 pl.xticks([-g, g])
-
-#pl.ylim(-1.0,1.0)
 pl.ylim(C.min()*1.1,C.max()*1.1)
-
-#pl.yticks(np.linspace(-1,1,5,endpoint=True))
 pl.yticks([-h, h])
-
 pl.title("Representacion grafica")
-
 pl.savefig("grafica.eps", dpi=72)
-
 pl.show()
